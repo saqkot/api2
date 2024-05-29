e73e558b43b02c7619c28b99d10da5f8b83acd43
saqkot <149250762+saqkot@users.noreply.github.com>
2024-05-29 01:45:41 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const cors = require('cors');
 
 
-
+app.use(cors());
 app.listen(app.get('port'), () =>{
 
     console.log("servidor escuchando en el puerto", app.get('port'));
