7641f1d17a1bf3c52ba62bab904fc96726bdb616
saqkot <149250762+saqkot@users.noreply.github.com>
2024-05-29 12:39:08 -0600

    Update rutas.js

--- src/modulos/solicitudes/rutas.js
+++ src/modulos/solicitudes/rutas.js
@@ -39,20 +39,15 @@ async function empleado(req, res){
    
     }; 
 
-    async function agregar(req, res){
-        try{
-         const items = await controlador.agregar(req.body);
-         if(req.body == 0){
-            mensaje = 'item guardado con exito';
-         }else{
-            mensaje = 'item actualizado con exito';
-         }
-         respuestas.success(req, res, mensaje,201);
-        }catch(err){
-         respuestas.error(req, res, err, 500);
-    
-    
-        }
+
+async function agregar(req, res) {
+    try {
+        const items = await controlador.agregar(req.body);
+        respuestas.success(req, res, 'Solicitud agregada correctamente', 201);
+    } catch (err) {
+        respuestas.error(req, res, err, 500);
+    }
+}
        
         }; 
     
