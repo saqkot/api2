eb89908c9d1a37a0aa93dab280c0b5ecfe9dcd0c
saqkot <149250762+saqkot@users.noreply.github.com>
2024-05-30 03:27:43 -0600

    Update controlador.js

--- src/modulos/usuarios/controlador.js
+++ src/modulos/usuarios/controlador.js
@@ -17,40 +17,39 @@ if(!db){
         return db.empleado(TABLA, id);
     }
     
-    
-     async function agregar(body){
-        const usuario ={
 
-            id:body.id,
-            nombre: body.nombre,
-            apellido:body.apellido,
-            tipo_usuario: body.tipo_usuario
+async function agregar(body) {
+    const usuario = {
+        id: body.id,
+        nombre: body.nombre,
+        apellido: body.apellido,
+        tipo_usuario: body.tipo_usuario
+    };
 
-        }
+    var respuestas = await db.agregar(TABLA, usuario);
+    console.log('respuestas', respuestas);
 
-        var respuestas = await db.agregar(TABLA, usuario);
-         console.log('respuestas',respuestas)
-        var insertId = 0;
-        if(body.id == 0){
-            insertId = respuestas.insertId;
-        }else{
-            insertId = body.id;
-
-        }
-        var repuesta2 = '';
-        if(body.usuario || body.password){
-         repuesta2 = await auth.agregar({
-                id: insertId,
-                usuario: body.usuario,
-                password: body.password
-
-            })
-
-        }
-
-        return repuesta2;
+    var insertId = 0;
+    if (body.id == 0) {
+        insertId = respuestas.insertId;
+    } else {
+        insertId = body.id;
     }
 
+    var respuesta2 = '';
+    if (body.usuario || body.password) {
+        respuesta2 = await auth.agregar({
+            id: insertId,
+            usuario: body.usuario,
+            password: body.password
+        });
+    }
+
+    return respuesta2;
+}
+
+
+    
     
     
  function eliminar(id) {
